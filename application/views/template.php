5aafa2e46e55ed52018918910bf2476159a76468
lizewu <lizewu@localhost>
2016-10-07 21:42:51 -0700

    add link

--- application/views/template.php
+++ application/views/template.php
@@ -31,7 +31,7 @@
                         <li class="active"><a href="/">Home</a></li>
                         <li><a href="menu">Menu</a></li>
                         <li><a href="inventory">Inventory</a></li>
-                        <li><a href="#">Recipe</a></li>
+                        <li><a href="recipe">Recipe</a></li>
                     </ul>
                     <ul class="nav navbar-nav navbar-right">
                         <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
