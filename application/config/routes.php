ce2aaa844fc44fff0b3415bc9d5d4ec947b9aab0
Lei Zhang(Daniel) <danielvmaple@gmail.com>
2016-10-07 21:28:58 -0700

    Created new controller and style pages
    Added a new controller controllers/inventory/Welcome.php , new model
    models/Inventories.php and new view views/inventory.php.
    Styled template.php using bootstrap.js and jquery.js

--- application/config/routes.php
+++ application/config/routes.php
@@ -54,3 +54,4 @@ $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['menu'] = 'menu/welcome/index';
 $route['menu/(:any)'] = 'menu/find/index/$1';
+$route['inventory'] = 'inventory/welcome/index';
---
+++ application/views/inventory.php
@@ -0,0 +1,6 @@
+<div id="inventory">
+    <h1>Inventory</h1>
+    {ingreds}
+    <p><a href=''>{name}</a></p>
+    {/ingreds}
+</div>
