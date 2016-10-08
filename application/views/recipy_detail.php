25c00f1e978509f750ed2540f9274788d619bf19
lizewu <lizewu@localhost>
2016-10-07 21:33:19 -0700

    add recipe view

---
+++ application/views/recipy_detail.php
@@ -0,0 +1,17 @@
+<h1>{name}</h1>
+    
+<div class="row">
+    <table style="width:100%">
+  <tr>
+    <th>Item</th> 
+    <th>Quantity</th>
+  </tr>
+  {detail}
+  <tr>
+    <td>{item}</td> 
+    <td>{Quantity}</td>
+  </tr>
+   {/detail}
+</table>
+   
+</div>
---
+++ application/views/recipy_list.php
@@ -0,0 +1,9 @@
+<div class="row">
+   <h1>Menu</h1>
+  
+    {names}
+        <a href="recipe/{id}">{name}</a>
+    {/names}
+
+   
+</div>
