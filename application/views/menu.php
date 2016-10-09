e9ede7cdcf1519d5cbeac17ddc5fbb866df745ed
Tianyang Liu <lty0711@qq.com>
2016-10-09 14:25:20 -0700

    Changed layout in view

--- application/views/menu.php
+++ application/views/menu.php
@@ -1,20 +1,38 @@
-<div id="main" style="width: 100%; border-bottom: solid 1px lightgray;">
-    <div id="drink" style="float: right; width: 50%;">
-        <div class="title">
-            <h1>Beverage</h1>
+<div class="panel panel-default">
+    <!-- Default panel contents -->
+    <div class="panel-heading" id="tableHeading">
+        <h1>Menu</h1>
+    </div>
+    <!-- Table -->
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Beverage</th>
+            </tr>
+        </thead>
+        <tbody>
             {drink}
-                <p><a href="menu{href}">{name}</a></p>
-            {/drink}
-        </div>
-    </div>
+            <tr>
+                <td><a href="menu{href}">{name}</a></td>
+            </tr>
+            {/drink}   
+        </tbody>
+    </table>
     
-    <div id="food" style="width: 50%">
-        <div class="title">
-            <h1>Food</h1>
+    <br>
+    
+    <table class="table">
+    <thead>
+            <tr>
+                <th>Food</th>
+            </tr>
+        </thead>
+        <tbody>
             {food}
-            <p><a href="menu{href}">{name}</a></p>
+            <tr>
+                <td><a href="menu{href}">{name}</a></td>
+            </tr>
             {/food}
-        </div>
-    </div>
+        </tbody>
+    </table>
 </div>
-<br>
