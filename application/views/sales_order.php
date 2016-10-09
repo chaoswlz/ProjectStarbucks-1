eef088d8ba6a29193dfb7b4b810e8fc3e73f3805
Tianyang Liu <lty0711@qq.com>
2016-10-09 14:21:51 -0700

    renamed menu controll
    renamed subcontroller Find.php in menu controll; also renamed view,
    which is related to that subcontroller

---
+++ application/views/sales_order.php
@@ -0,0 +1,22 @@
+
+<div class="panel panel-default">
+    <div class="panel-heading" id="tableHeading">
+        <h1>{name}</h1>
+    </div>
+    
+    <!-- Table -->
+    <table class="table">
+        <thead>
+            <tr>
+              <th>Price</th>
+              <th>Stock</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>${price}</td>
+                <td>{rest}</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
